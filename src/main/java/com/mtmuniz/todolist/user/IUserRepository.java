8139ea0a25fc786c816ec121af589065f3c6ed9e
Matheus Muniz <mmuniz605@gmail.com>
2023-10-14 17:35:19 -0300

    finish project

---
+++ src/main/java/com/mtmuniz/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.mtmuniz.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String name);
+}
